298752487a858f999623b60b29fe6d0af30c80be
LianaMi <liana.mirzojan@gmail.com>
2021-03-18 09:58:44 +0100

    Update18_03

---
+++ 11planets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="11planets" tilewidth="32" tileheight="32" tilecount="35" columns="5">
+ <image source="../../../Desktop/11planets.jpg" width="191" height="250"/>
+</tileset>
